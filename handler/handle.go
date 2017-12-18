f2cc7fc684fb91d76508fee0cf8f1bdd02076f4e
Manfred Touron <m@42.am>
2017-12-18 20:50:24 +0100

    Mv handlers to their own package

---
+++ handler/handle.go
@@ -0,0 +1,109 @@
+package handler
+
+import (
+	"io"
+	"math/rand"
+	"time"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+
+	pb "github.com/moul/grpcbin/grpcbin"
+)
+
+type Handlers struct{}
+
+func (h *Handlers) Index(ctx context.Context, in *pb.EmptyMessage) (*pb.IndexReply, error) {
+	reply := pb.IndexReply{
+		Description: "gRPC testing server",
+		Endpoints:   []*pb.IndexReply_Endpoint{},
+	}
+	for _, method := range pb.GRPCBin_serviceDesc.Methods {
+		reply.Endpoints = append(reply.Endpoints, &pb.IndexReply_Endpoint{
+			Path: method.MethodName,
+			// Description: FIXME get from comments
+		})
+	}
+	return &reply, nil
+}
+
+func (h *Handlers) HeadersUnary(ctx context.Context, in *pb.EmptyMessage) (*pb.HeadersMessage, error) {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return nil, status.Error(codes.InvalidArgument, "cannot parse metadata from incoming context")
+	}
+	resp := pb.HeadersMessage{
+		Metadata: map[string]*pb.HeadersMessage_Values{},
+	}
+	for key, values := range md {
+		resp.Metadata[key] = &pb.HeadersMessage_Values{Values: values}
+	}
+	return &resp, nil
+}
+
+func (h *Handlers) NoResponseUnary(ctx context.Context, in *pb.EmptyMessage) (*pb.EmptyMessage, error) {
+	return nil, nil
+}
+
+func (h *Handlers) Empty(ctx context.Context, in *pb.EmptyMessage) (*pb.EmptyMessage, error) {
+	return &pb.EmptyMessage{}, nil
+}
+
+func (h *Handlers) DummyUnary(ctx context.Context, in *pb.DummyMessage) (*pb.DummyMessage, error) {
+	return in, nil
+}
+
+func (h *Handlers) RandomError(ctx context.Context, in *pb.EmptyMessage) (*pb.EmptyMessage, error) {
+	c := codes.Code(uint32(rand.Intn(16)))
+	return &pb.EmptyMessage{}, status.Error(c, c.String())
+}
+
+func (h *Handlers) SpecificError(ctx context.Context, in *pb.SpecificErrorRequest) (*pb.EmptyMessage, error) {
+	c := codes.Code(in.Code)
+	msg := c.String()
+	if in.Reason != "" {
+		msg = in.Reason
+	}
+	return &pb.EmptyMessage{}, status.Error(c, msg)
+}
+
+func (h *Handlers) DummyBidirectionalStreamStream(stream pb.GRPCBin_DummyBidirectionalStreamStreamServer) error {
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return err
+		}
+		if err := stream.Send(req); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (h *Handlers) DummyClientStream(stream pb.GRPCBin_DummyClientStreamServer) error {
+	var req *pb.DummyMessage
+	var err error
+	for i := 0; i < 10; i++ {
+		req, err = stream.Recv()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return err
+		}
+	}
+	return stream.SendAndClose(req)
+}
+
+func (h *Handlers) DummyServerStream(in *pb.DummyMessage, stream pb.GRPCBin_DummyServerStreamServer) error {
+	for i := 0; i < 10; i++ {
+		if err := stream.Send(in); err != nil {
+			return err
+		}
+		time.Sleep(1 * time.Second)
+	}
+	return nil
+}
--- main.go
+++ main.go
@@ -1,116 +1,15 @@
 package main
 
 import (
-	"io"
 	"log"
-	"math/rand"
 	"net"
-	"time"
 
-	"golang.org/x/net/context"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/reflection"
-	"google.golang.org/grpc/status"
 
 	pb "github.com/moul/grpcbin/grpcbin"
+	"github.com/moul/grpcbin/handler"
 )
-
-type server struct{}
-
-func (s *server) Index(ctx context.Context, in *pb.EmptyMessage) (*pb.IndexReply, error) {
-	reply := pb.IndexReply{
-		Description: "gRPC testing server",
-		Endpoints:   []*pb.IndexReply_Endpoint{},
-	}
-	for _, method := range pb.GRPCBin_serviceDesc.Methods {
-		reply.Endpoints = append(reply.Endpoints, &pb.IndexReply_Endpoint{
-			Path: method.MethodName,
-			// Description: FIXME get from comments
-		})
-	}
-	return &reply, nil
-}
-
-func (s *server) HeadersUnary(ctx context.Context, in *pb.EmptyMessage) (*pb.HeadersMessage, error) {
-	md, ok := metadata.FromIncomingContext(ctx)
-	if !ok {
-		return nil, status.Error(codes.InvalidArgument, "cannot parse metadata from incoming context")
-	}
-	resp := pb.HeadersMessage{
-		Metadata: map[string]*pb.HeadersMessage_Values{},
-	}
-	for key, values := range md {
-		resp.Metadata[key] = &pb.HeadersMessage_Values{Values: values}
-	}
-	return &resp, nil
-}
-
-func (s *server) NoResponseUnary(ctx context.Context, in *pb.EmptyMessage) (*pb.EmptyMessage, error) {
-	return nil, nil
-}
-
-func (s *server) Empty(ctx context.Context, in *pb.EmptyMessage) (*pb.EmptyMessage, error) {
-	return &pb.EmptyMessage{}, nil
-}
-
-func (s *server) DummyUnary(ctx context.Context, in *pb.DummyMessage) (*pb.DummyMessage, error) {
-	return in, nil
-}
-
-func (s *server) RandomError(ctx context.Context, in *pb.EmptyMessage) (*pb.EmptyMessage, error) {
-	c := codes.Code(uint32(rand.Intn(16)))
-	return &pb.EmptyMessage{}, status.Error(c, c.String())
-}
-
-func (s *server) SpecificError(ctx context.Context, in *pb.SpecificErrorRequest) (*pb.EmptyMessage, error) {
-	c := codes.Code(in.Code)
-	msg := c.String()
-	if in.Reason != "" {
-		msg = in.Reason
-	}
-	return &pb.EmptyMessage{}, status.Error(c, msg)
-}
-
-func (s *server) DummyBidirectionalStreamStream(stream pb.GRPCBin_DummyBidirectionalStreamStreamServer) error {
-	for {
-		req, err := stream.Recv()
-		if err == io.EOF {
-			break
-		} else if err != nil {
-			return err
-		}
-		if err := stream.Send(req); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func (s *server) DummyClientStream(stream pb.GRPCBin_DummyClientStreamServer) error {
-	var req *pb.DummyMessage
-	var err error
-	for i := 0; i < 10; i++ {
-		req, err = stream.Recv()
-		if err == io.EOF {
-			break
-		} else if err != nil {
-			return err
-		}
-	}
-	return stream.SendAndClose(req)
-}
-
-func (s *server) DummyServerStream(in *pb.DummyMessage, stream pb.GRPCBin_DummyServerStreamServer) error {
-	for i := 0; i < 10; i++ {
-		if err := stream.Send(in); err != nil {
-			return err
-		}
-		time.Sleep(1 * time.Second)
-	}
-	return nil
-}
 
 func main() {
 	listener, err := net.Listen("tcp", ":9000")
@@ -118,7 +17,7 @@ func main() {
 		log.Fatalf("failted to listen: %v", err)
 	}
 	s := grpc.NewServer()
-	pb.RegisterGRPCBinServer(s, &server{})
+	pb.RegisterGRPCBinServer(s, &handler.Handlers{})
 	// register reflection service on gRPC server
 	reflection.Register(s)
 	log.Println("listening on :9000")
