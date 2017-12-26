9e86328cdc8eef8c33802d094eba883a9bf53d39
Manfred Touron <m@42.am>
2017-12-26 11:30:41 +0100

    Testing handlers

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ install:
 	go install -v .
 
 test:
-	go test -v .
+	go test -v . ./handler/...
 
 lint:
 	gometalinter --disable-all --enable=errcheck --enable=vet --enable=vetshadow --enable=golint --enable=gas --enable=ineffassign --enable=goconst --enable=goimports --enable=gofmt --exclude="Binds to all network interfaces" --exclude="should have comment" --enable=staticcheck --enable=gosimple --enable=misspell --deadline=20s .
---
+++ handler/handle_test.go
@@ -0,0 +1,75 @@
+package handler
+
+import (
+	"context"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+
+	pb "github.com/moul/pb/grpcbin/go-grpc"
+)
+
+func TestHandlers(t *testing.T) {
+	Convey("Testing Handlers{}", t, func() {
+		s := Handlers{}
+		ctx := context.Background()
+
+		Convey("Testing Index()", func() {
+			resp, err := s.Index(ctx, &pb.EmptyMessage{})
+			So(err, ShouldBeNil)
+			So(resp, ShouldNotBeNil)
+			So(len(resp.Endpoints) > 0, ShouldBeTrue)
+			So(len(resp.Endpoints[0].Path) > 0, ShouldBeTrue)
+		})
+
+		Convey("Testing Empty()", func() {
+			resp, err := s.Empty(ctx, &pb.EmptyMessage{})
+			So(err, ShouldBeNil)
+			So(resp, ShouldNotBeNil)
+		})
+
+		Convey("Testing DummyUnary()", func() {
+			req := &pb.DummyMessage{
+				FString: "hello world",
+				FInt32:  42,
+			}
+			resp, err := s.DummyUnary(ctx, req)
+			So(err, ShouldBeNil)
+			So(resp.FString, ShouldEqual, "hello world")
+			So(resp.FInt32, ShouldEqual, 42)
+			So(resp, ShouldResemble, req)
+		})
+
+		Convey("Testing RandomError()", func() {
+			_, err := s.RandomError(ctx, &pb.EmptyMessage{})
+			So(err, ShouldNotBeNil)
+		})
+
+		Convey("Testing SpecificError()", func() {
+			_, err := s.SpecificError(ctx, &pb.SpecificErrorRequest{
+				Code: 1,
+			})
+			So(err, ShouldNotBeNil)
+			So(err.Error(), ShouldEqual, "rpc error: code = Canceled desc = Canceled")
+
+			_, err = s.SpecificError(ctx, &pb.SpecificErrorRequest{
+				Code: 2,
+			})
+			So(err, ShouldNotBeNil)
+			So(err.Error(), ShouldEqual, "rpc error: code = Unknown desc = Unknown")
+
+			_, err = s.SpecificError(ctx, &pb.SpecificErrorRequest{
+				Code:   3,
+				Reason: "lorem ipsum",
+			})
+			So(err, ShouldNotBeNil)
+			So(err.Error(), ShouldEqual, "rpc error: code = InvalidArgument desc = lorem ipsum")
+		})
+
+		// Convey("Testing DummyBidirectionalStreamStream()", func() {})
+		// Convey("Testing DummyClientStream()", func() {})
+		// Convey("Testing DummyServerStream()", func() {})
+		// Convey("Testing HeadersUnary()", func() {})
+		// Convey("Testing NoResponseUnary()", func() {})
+	})
+}
